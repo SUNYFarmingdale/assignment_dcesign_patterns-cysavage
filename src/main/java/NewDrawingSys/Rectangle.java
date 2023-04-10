e20e3a7fc54383652692cb8f5d04f808db88af64
Cyril K Thomas <cyrilkthomasest.1991@gmail.com>
2023-04-10 19:39:36 -0400

    Rectangle Class

---
+++ src/main/java/NewDrawingSys/Rectangle.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package NewDrawingSys;
+
+/**
+ *Blueprint for rectangle class - create instances of rectangles
+ * @author cyril
+ */
+public class Rectangle {
+    
+
+
+     public void draw(int x, int y, int width, int height) {
+        System.out.println("Rectangle with coordinates:(" + x + ";" + y + "), width: " + width
+                + ", height: " + height);
+    }
+    
+}
