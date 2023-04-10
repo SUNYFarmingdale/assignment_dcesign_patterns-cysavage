7d18a52c5b914189e4d7de0cfe9d3ee6d2a2f721
Cyril K Thomas <cyrilkthomasest.1991@gmail.com>
2023-04-10 19:40:52 -0400

    Shape interface Class

---
+++ src/main/java/NewDrawingSys/Shape.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package NewDrawingSys;
+
+/**
+ * Base Class Shape - Classes inherit
+ * @author cyril
+ */
+public interface Shape {
+    void draw(int x1, int y1, int x2, int y2);
+}
