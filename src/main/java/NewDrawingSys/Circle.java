179b0637ac7473732f2b203db4d68a8dd4bfa7fb
Cyril K Thomas <cyrilkthomasest.1991@gmail.com>
2023-04-10 19:38:19 -0400

    circle class

---
+++ src/main/java/NewDrawingSys/Circle.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package NewDrawingSys;
+
+/**
+ *Blueprint for circle class
+ * @author cyril
+ */
+public class Circle {
+    
+    Circle(){
+        
+    }
+
+
+    //Draw method
+
+public void draw(int x1, int y1, int r){
+System.out.println("Circle from center: (" +x1 +";" + y1 + "), with radius(" + r +")");
+}
+}
