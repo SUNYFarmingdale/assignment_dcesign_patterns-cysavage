fbb9c1fc1e6a7c2252c0606834ea97d01c5bb9db
Cyril K Thomas <cyrilkthomasest.1991@gmail.com>
2023-04-10 19:36:13 -0400

    changes to modifiedsystem

--- src/main/java/NewDrawingSys/ModifiedSystem.java
+++ src/main/java/NewDrawingSys/ModifiedSystem.java
@@ -5,14 +5,17 @@
 package NewDrawingSys;
 
 /**
- *
+ *Driver File - Edited - cyril
  * @author MoaathAlrajab
  */
 public class ModifiedSystem {
     public static void main(String[] args) {
-        Shape[] shapes = //add your code here;
+        //Add created shapes to existing code 
+        Shape[] shapes = {new RectAdapter(new NewDrawingSys.Rectangle()), new CAdapter(new NewDrawingSys.Circle())};
         int x1 = 10, y1 = 20;
         int x2 = 30, y2 = 60;
+        
+        
         for (Shape shape : shapes) {
             shape.draw(x1, y1, x2, y2);
         }
